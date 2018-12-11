c8a9ac95f770d5e0208ff870c41baee0d2dc831c
Jared Gibson <jared.gibson@live.com>
2018-12-11 11:28:02 -0700

    Add project files.

---
+++ Project2_MissionaryApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Project2_MissionaryApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2_MissionaryApp.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Contact");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Project2_MissionaryApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project2_MissionaryApp.Startup))]
+namespace Project2_MissionaryApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
