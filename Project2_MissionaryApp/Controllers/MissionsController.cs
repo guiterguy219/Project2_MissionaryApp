e48e0b799ac93655e3206203feeeefc06d34c12f
Jared Gibson <jared.gibson@live.com>
2018-12-12 10:15:27 -0700

    delete auth and dbContext stuff

--- Project2_MissionaryApp/Controllers/MissionsController.cs
+++ Project2_MissionaryApp/Controllers/MissionsController.cs
@@ -1,5 +1,4 @@
 ﻿using Project2_MissionaryApp.Controllers;
-using Project2_MissionaryApp.DAL;
 using Project2_MissionaryApp.Models;
 using System;
 using System.Collections.Generic;
@@ -12,14 +11,13 @@ namespace MissionaryApp.Controllers
     [Authorize]
     public class MissionsController : AccountController
     {
-        private static MissionContext db = new MissionContext();
         // GET: Missions
         [AllowAnonymous]
         [Route("Missions")]
         [Route("Missions/Index")]
         public ActionResult Index()
         {
-            return View(db.Mission.ToList());
+            return View();
         }
 
         [AllowAnonymous]
@@ -29,7 +27,7 @@ namespace MissionaryApp.Controllers
             {
                 return RedirectToAction("Index");
             }
-            return View(db.Mission.Find(id));
+            return View();
         }
 
         [HttpPost]
@@ -37,17 +35,7 @@ namespace MissionaryApp.Controllers
         {
             if (form["question"] != null && form["question"] != String.Empty)
             {
-                MissionQuestions question = new MissionQuestions();
-                question.MissionID = id;
-                question.Question = form["question"];
-                question.UserID = User.Identity.Name;
 
-                db.MissionQuestions.Add(question);
-                db.SaveChanges();
-            }
-            else
-            {
-                ModelState.AddModelError("question", "Please enter a question.");
             }
 
             return RedirectToAction("Faqs", routeValues: new { id = id });
@@ -58,18 +46,9 @@ namespace MissionaryApp.Controllers
         {
             if (form["answer_" + questionID] != null && form["answer_" + questionID] != String.Empty)
             {
-                MissionAnswers answer = new MissionAnswers();
-                answer.MissionQuestionID = questionID;
-                answer.Answer = form["answer_" + questionID];
-                answer.UserID = User.Identity.Name;
 
-                db.MissionAnswers.Add(answer);
-                db.SaveChanges();
             }
             else
-            {
-                ModelState.AddModelError("response", "Please enter your response.");
-            }
 
             return RedirectToAction("Faqs", routeValues: new { id = missionID });
         }
@@ -78,15 +57,7 @@ namespace MissionaryApp.Controllers
         {
             if(uId == User.Identity.Name)
             {
-                foreach(MissionAnswers answer in db.MissionAnswers.Where(a => a.MissionQuestionID == qId))
-                {
-                    db.MissionAnswers.Remove(answer);
-                    db.SaveChanges();
-                }
 
-                MissionQuestions question = db.MissionQuestions.Find(qId);
-                db.MissionQuestions.Remove(question);
-                db.SaveChanges();  
             }
 
             return RedirectToAction("Faqs", routeValues: new { id = missionId });
@@ -96,9 +67,7 @@ namespace MissionaryApp.Controllers
         {
             if (uId == User.Identity.Name)
             {
-                MissionAnswers answer = db.MissionAnswers.Find(aId);
-                db.MissionAnswers.Remove(answer);
-                db.SaveChanges();
+
             }
 
             return RedirectToAction("Faqs", routeValues: new { id = missionId });
